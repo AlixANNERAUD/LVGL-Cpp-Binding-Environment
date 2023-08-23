e1c10f3d8e886634fa8505f8d7ebabd1ecbfc291
Alix ANNERAUD <alix.anneraud@outlook.fr>
2023-08-23 20:43:10 +0200

    Configure to make it working with lv_binding_cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,33 @@
 
 #include "demos/lv_demos.h"
 
+#include <stdio.h>
+
+#include "LVGL_Cpp.hpp"
+
 int main(void)
 {
 	lv_init();
 
 	hal_setup();
 
-  lv_demo_widgets();
+  //lv_demo_widgets();
+
+  LVGL::Object_Type Screen(lv_scr_act());
+
+  LVGL::Button_Type B(Screen);
+  B.Set_Align(LV_ALIGN_CENTER);
+
+  LVGL::Label_Type Label(B);
+
+  LVGL::Switch_Type S(Screen);
+  S.Set_Align(LV_ALIGN_BOTTOM_LEFT);
+  
+  LVGL::Colorwheel_Type C(Screen, true);
+  C.Set_Align(LV_ALIGN_BOTTOM_RIGHT);
+  C.Set_Size(200, 200);
+
+  Label.Set_Text("Hello World!");
 
 	hal_loop();
 }
